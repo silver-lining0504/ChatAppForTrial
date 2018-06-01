a0d33248619c61de22ce8e40fe1fd8c5ff693a72
silver-lining0504 <k0254@hotmail.co.jp>
2018-06-01 16:39:47 +0900

    Initial Commit

---
+++ ChatAppForTrial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatAppForTrial
+//
+//  Created by Oneinch on 2018/06/01.
+//  Copyright © 2018年 Rina Unno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
