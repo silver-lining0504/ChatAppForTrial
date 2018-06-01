ae9fc77fdd5960c6f8995bc5ae29959c49e25fc7
silver-lining0504 <k0254@hotmail.co.jp>
2018-06-01 17:27:03 +0900

    アップロードテスト

--- ChatAppForTrial/ViewController.swift
+++ ChatAppForTrial/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var messageField: UITextField!
+    
+    
+    var databaseRef:DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        messageField.delegate = self
+        
+        databaseRef = Database.database().reference()
+        
+        databaseRef.observe(DataEventType.childAdded, with: { snapshot in
+            if let name = (snapshot.value! as AnyObject).object(forKey: "name") as? String,
+                let message = (snapshot.value! as AnyObject).object(forKey: "message") as? String {
+                self.textView.text! = "\(self.textView.text!)\n\(name) : \(message)"
+            }
+        })
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        
+        let messageData = ["name": nameField.text!, "message": messageField.text!]
+        
+        databaseRef.childByAutoId().setValue(messageData)
+        
+        textField.resignFirstResponder()
+        messageField.text = ""
+        
+        return true
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
